fb6c4865bec1cbc6e426e6eb886cd59ceddbf363
eamine <elmallem.amine@gmail.com>
2016-07-12 06:12:56 +0100

    Correction nom de variable

--- src/main/java/fr/softeam/kata/romanNumerals/RomanNumeral.java
+++ src/main/java/fr/softeam/kata/romanNumerals/RomanNumeral.java
@@ -5,7 +5,7 @@ import org.apache.commons.lang3.StringUtils;
 public class RomanNumeral {
 	
 	private static final int[] ARABIC_NUMBERS  = { 1000,900,500,400,100,90,50,40,10,9,5,4,1 };
-	private static final String[] ROMAN_NUMBERS = { "M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I" };
+	private static final String[] ROMAN_NUMERALS = { "M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I" };
 
 	/**
 	 * This function converts an Arabic number to a Roman one
@@ -14,14 +14,14 @@ public class RomanNumeral {
 	 */
 	public static String convertArabicToRoman(int arabicNumber) {
 		
-		String romanNumber = StringUtils.EMPTY;
+		String romanNumeral = StringUtils.EMPTY;
 		
 	      for (int i = 0; i < ARABIC_NUMBERS.length; i++) {
 	         while (arabicNumber >= ARABIC_NUMBERS[i]) {
-	        	 romanNumber += ROMAN_NUMBERS[i];
+	        	 romanNumeral += ROMAN_NUMERALS[i];
 	        	 arabicNumber -= ARABIC_NUMBERS[i];
 	         }
 	      }
-	      return romanNumber;
+	      return romanNumeral;
 	}
 }
